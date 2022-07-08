25f9f25cdaa931e5877127bbc583bbd2f2a5526f
ww <1320917731@qq.com>
2022-07-08 17:03:23 +0800

    ww：增加dagger依赖

--- app/src/main/java/com/tikou/supportproject/MainActivity.kt
+++ app/src/main/java/com/tikou/supportproject/MainActivity.kt
@@ -2,10 +2,13 @@ package com.tikou.supportproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
